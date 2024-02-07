6dacc8429263b33105ff4b7526e57677e84a12a7
Mikko Kokkonen <mikko@mikian.com>
2024-02-07 23:05:25 +0100

    feat: add JSON output

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "simplecov"
+gem "simplecov-json"
 gem "simplecov-lcov"
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,13 @@ SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.collate Dir["#{ENV['COVERAGE_PATH']}/*.json"], "rails" do
   formatter SimpleCov::Formatter::MultiFormatter.new(
     [
-      SimpleCov::Formatter::LcovFormatter
+      SimpleCov::Formatter::LcovFormatter,
+      SimpleCov::Formatter::JsonFormatter
     ]
   )
 end
 
 File.open(ENV["GITHUB_OUTPUT"], "w+") do |out|
+  out.puts "json_path=#{SimpleCov.coverage_path}/coverage.json"
   out.puts "lcov_path=#{SimpleCov::Formatter::LcovFormatter.config.single_report_path}"
 end
